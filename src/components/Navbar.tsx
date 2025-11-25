b06bf380e7ad8974fdf070c21c0545e6b22a0dbc
William Justice Davis <hvusvztc770@gmail.com>
2025-11-25 15:44:28 +0200

    First commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "@constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">William's portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("dd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
